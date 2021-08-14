0da765219f17c50aab12ca28160ed311cc771315
Patrizio Gasperi <patrizio.gasperi@gmail.com>
2021-08-14 13:26:08 +0200

    fine implementazione

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,23 +1,20 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_meals_app/models/categories_fixture.dart';
 
-class MealDetailScreen extends StatefulWidget {
-  static const String routeName = '/meal-detail';
+class MealDetailScreen extends StatelessWidget {
+  static const routeName = '/meal-detail';
 
-  const MealDetailScreen({Key? key}) : super(key: key);
+  final Function toggleFavorite;
+  final Function isFavorite;
 
-  @override
-  _MealDetailScreenState createState() => _MealDetailScreenState();
-}
+  MealDetailScreen(this.toggleFavorite, this.isFavorite);
 
-class _MealDetailScreenState extends State<MealDetailScreen> {
-  Widget buildSectionTitle(BuildContext context, String title) {
+  Widget buildSectionTitle(BuildContext context, String text) {
     return Container(
       margin: EdgeInsets.symmetric(vertical: 10),
       child: Text(
-        title,
-        style: Theme.of(context).textTheme.headline6,
+        text,
+        style: Theme.of(context).textTheme.title,
       ),
     );
   }
@@ -25,12 +22,13 @@ class _MealDetailScreenState extends State<MealDetailScreen> {
   Widget buildContainer(Widget child) {
     return Container(
       decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular((10))),
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
       margin: EdgeInsets.all(10),
       padding: EdgeInsets.all(10),
-      height: 200,
+      height: 150,
       width: 300,
       child: child,
     );
@@ -38,14 +36,15 @@ class _MealDetailScreenState extends State<MealDetailScreen> {
 
   @override
   Widget build(BuildContext context) {
-    var id = ModalRoute.of(context)!.settings.arguments as String;
-    final selectedMeal = DUMMY_MEALS.firstWhere((element) => element.id == id);
-
+    final mealId = ModalRoute.of(context)!.settings.arguments as String;
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
     return Scaffold(
-      appBar: AppBar(title: Text('${selectedMeal.title}')),
+      appBar: AppBar(
+        title: Text('${selectedMeal.title}'),
+      ),
       body: SingleChildScrollView(
         child: Column(
-          children: [
+          children: <Widget>[
             Container(
               height: 300,
               width: double.infinity,
@@ -57,34 +56,45 @@ class _MealDetailScreenState extends State<MealDetailScreen> {
             buildSectionTitle(context, 'Ingredients'),
             buildContainer(
               ListView.builder(
-                  itemCount: selectedMeal.ingredients.length,
-                  itemBuilder: (ctx, index) {
-                    return Card(
+                itemBuilder: (ctx, index) => Card(
                       color: Theme.of(context).accentColor,
                       child: Padding(
-                        padding:
-                            EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                        child: Text(selectedMeal.ingredients[index]),
-                      ),
-                    );
-                  }),
+                          padding: EdgeInsets.symmetric(
+                            vertical: 5,
+                            horizontal: 10,
+                          ),
+                          child: Text(selectedMeal.ingredients[index])),
+                    ),
+                itemCount: selectedMeal.ingredients.length,
+              ),
             ),
             buildSectionTitle(context, 'Steps'),
-            buildContainer(ListView.builder(
-                itemCount: selectedMeal.steps.length,
+            buildContainer(
+              ListView.builder(
                 itemBuilder: (ctx, index) => Column(
                       children: [
                         ListTile(
                           leading: CircleAvatar(
                             child: Text('# ${(index + 1)}'),
                           ),
-                          title: Text(selectedMeal.steps[index]),
+                          title: Text(
+                            selectedMeal.steps[index],
+                          ),
                         ),
-                        Divider(),
+                        Divider()
                       ],
-                    )))
+                    ),
+                itemCount: selectedMeal.steps.length,
+              ),
+            ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+           isFavorite(mealId) ? Icons.star : Icons.star_border,
+        ),
+        onPressed: () => toggleFavorite(mealId),
       ),
     );
   }
